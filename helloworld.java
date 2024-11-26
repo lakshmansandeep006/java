353f04802d559bc29e6adf296bf6129e6a1a382f
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2024-11-26 10:54:08 +0000

    java6

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 
 
+
 public class helloworld {
   public static void main(String[] args) {
-    System.out.println("Hello sandeep  reddy");
+    System.out.println("hHello sandeep  reddy");
   }
 }
